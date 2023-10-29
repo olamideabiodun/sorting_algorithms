d66ae6b44a2cef283b3963928e243f366a2de1a5
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2023-10-29 22:34:24 +0200

    insertion

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+
+/**
+ * swap_list - swap node in a doubly linked list
+ * @h: a pointer to the head of the list
+ * @n1: first node to swap
+ * @n2: second node to swap
+*/
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
+
+/**
+ * insertion_sort_list - sorts doubly linked list with insertion sort
+ * @list: list to sort
+*/
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *current, *run, *tmp;
+
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
+    for (current = (*list)->next; current != NULL; current = tmp)
+	{
+		tmp = current->next;
+		run = current->prev;
+		while (run != NULL && current->n < run->n)
+		{
+			swap_nodes(list, &run, current);
+			print_list((const listint_t *)*list);
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -31,6 +31,8 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
+void insertion_sort_list(listint_t **list);
 int main(void);
 void swap_ints(int *a, int *b);
 
